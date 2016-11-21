12bbfbcce11e15f6d8b59ab9743d60ab155d4c8d
Felipe Francisco <felipefrancisco.cwb@mail.com>
2016-11-21 10:58:08 -0200

    Added Some things

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,13 @@
 $this->addController('HomeController');
 $this->get('/', 'controller.home:indexPage');
 
+$this->get('/assets/all.js', function () {
+    return file_get_contents(__DIR__ . '/../public/assets/all.js');
+});
+$this->get('/assets/styles.css', function () {
+    return file_get_contents(__DIR__ . '/../public/assets/styles.css');
+});
+
 $this->mount('/api', function ($api) {
     $this->addController('PositionController');
 
--- views/position.php
+++ views/position.php
@@ -4,8 +4,9 @@
         <title>Vagas - Catho</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.min.css">
+        <link rel="stylesheet" type="text/css" href="/assets/styles.css">
     </head>
-    <body>
+    <body id="root">
         <div class="ui raised very padded container segment">
             <div class="ui header grid">
                 <div class="six wide column"></div>
@@ -14,14 +15,45 @@
                 </div>
             </div>
             <hr />
-            <br /><br />
+            <br />
             <div id="intern" class="ui grid">
+                <div class="three wide column"></div>
+                <div class="ten wide column">
+                    <form class="ui form">
+                        <div class="twelve fields">
+                            <div class="three wide field">
+                                <select class="ui fluid dropdown">
+                                    <option value="title">Titulo</option>
+                                    <option value="description">Descrição</option>
+                                </select>
+                            </div>
+                            <div class="ten wide field">
+                                <input type="text" name='search' placeholder="Pesquisar" />
+                            </div>
+                            <div class="field">
+                                <button class="ui button primary">Pesquisar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <br>
+
+                <article id="positions" class="twelve wide column">
+                    <section class="position">
+                        <h4>Vaga</h4>
+                        <b>Cidade - UF</b><br>
+                        <b>Salário: </b> <span style="ui red">$ 3.000,00</span>
+                        <blockquote class="details">
+                            
+                        </blockquote>
+                    </section>
+                </article>
 
             </div>
         </div>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.min.js"></script>
-        <script src="/assets/dist/all.js"></script>
+        <script src="/assets/all.js"></script>
     </body>
 </html>
